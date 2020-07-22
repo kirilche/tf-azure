015666355e91472bc21b23c42035fc7abd9313c1
Kiril Chervoniak <kiril.chervoniak@gmail.com>
2020-07-22 16:00:14 +0000

    providers with SP and Secret auth

---
+++ providers.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  version = "~>2.0"
+  subscription_id = var.azure-subscription-id
+  client_id       = var.azure-client-id
+  client_secret   = var.azure-client-secret 
+  tenant_id       = var.azure-tenant-id
+}
